51c4cd681b8eaa3ed69fa35f44e1a6f05ea033f2
Emily Bowersox <emily.bowersox100@gmail.com>
2020-05-19 11:43:17 -0500

    fixed version type by adding properties to pom file

---
+++ src/main/java/StringUtils.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class StringUtils {
+//extends object
+    public static void main(String[] args) {
+
+        System.out.println("Tell me something!");
+        Scanner scanner = new Scanner(System.in);
+//        String userInput = scanner.next();
+        System.out.println("You entered: " + scanner.next());
+
+    }
+
+
+
+}
